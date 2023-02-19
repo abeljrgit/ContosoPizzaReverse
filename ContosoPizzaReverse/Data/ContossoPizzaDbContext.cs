76d2fc28cb4b7c78d6b8dcbe2dad76b3cb0b9d64
ALKHWARIZMI-PC\abelm <abelmanuellugtujr1996@gmail.com>
2023-02-19 11:39:39 +0800

    Add project files.

---
+++ ContosoPizzaReverse/Data/ContossoPizzaDbContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using ContossoPizza.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContossoPizza.Data;
+
+public partial class ContossoPizzaDbContext : DbContext
+{
+    public ContossoPizzaDbContext()
+    {
+    }
+
+    public ContossoPizzaDbContext(DbContextOptions<ContossoPizzaDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    public virtual DbSet<Order> Orders { get; set; }
+
+    public virtual DbSet<OrderDetail> OrderDetails { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=ContossoPizzaDb;Trusted_Connection=True;Integrated Security=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ ContosoPizzaReverse/Program.cs
@@ -0,0 +1,9 @@
+﻿using ContossoPizza.Data;
+using ContossoPizza.Models;
+
+using ContossoPizzaDbContext context = new ContossoPizzaDbContext();
+
+foreach (Product p in context.Products)
+{
+    Console.WriteLine($"Id: {p.Id} Name: {p.Name}");
+}
